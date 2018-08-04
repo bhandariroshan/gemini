f1e25346a543ba5768133e66190e70d375da8d1a
Roshan <brishi98@gmail.com>
2018-08-04 20:33:15 +0545

    removed

--- app/src/main/java/com/example/rbhandari/datasyncapplication/datahandler/FeatureHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.rbhandari.datasyncapplication.datahandler;
-
-import org.json.JSONObject;
-
-public class FeatureHandler {
-    private static String auditClassName = "FeatureMain";
-    private static JSONObject auditData = new JSONObject();
-
-}
